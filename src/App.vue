e9b204ae94e307b21ebd5db9e1a21dba30dd2117
Alexi Taylor <alexi@bluesunrise.com>
2018-03-16 16:31:22 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+   <Nav/>
+    <div id="app">
+      <b-container>
+        <Cards/>
+        <Table/>
+      <!--<router-view/>-->
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cards from './components/Cards';
+import Nav from './components/Nav';
+import Table from './components/Table';
+
+export default {
+  name: 'App',
+  components: {
+    Cards,
+    Nav,
+    Table,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './assets/styles/custom-bootstrap.scss';
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-table striped hover :items="items" :fields="fields"></b-table>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  data() {
+    return {
+      // Note 'isActive' is left out and will not appear in the rendered table
+      fields: {
+        last_name: {
+          label: 'Person last name',
+          sortable: true,
+        },
+        first_name: {
+          label: 'Person first name',
+          sortable: false,
+        },
+        foo: {
+          // This key overrides `foo`!
+          key: 'age',
+          label: 'Person age',
+          sortable: true,
+        },
+        city: {
+          key: 'address.city',
+        },
+        'address.country': {
+          label: 'Country',
+        },
+      },
+      items: [
+        { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald', address: { country: 'USA', city: 'New York' } },
+        { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw', address: { country: 'Canada', city: 'Toronto' } },
+        { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson', address: { country: 'Australia', city: 'Sydney' } },
+        { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney', address: { country: 'England', city: 'London' } },
+      ],
+    };
+  },
+};
+</script>
